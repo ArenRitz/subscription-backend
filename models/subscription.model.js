cbfadd6f0ad7482798589852d0e49ef433991e5d
ArenRitz <aren.dolbakian@gmail.com>
2025-04-11 16:07:34 -0400

    Added error handling middleware

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -80,4 +80,10 @@ subscriptionSchema.pre('save', function (next) {
         this.status = 'expired';
     };
 
+    next();
+
 })
+
+const Subscription = mongoose.model('Subscription', subscriptionSchema);
+
+export default Subscription;
